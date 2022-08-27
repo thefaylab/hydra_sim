3445189cd8637853f15c425d39002bf96811576a
Sarah Gaichas <sarah.gaichas@noaa.gov>
2022-08-26 20:36:41 -0400

    run with hand fixed 5 bin

--- run-hydra.sh
+++ run-hydra.sh
@@ -1,4 +1,6 @@
 #./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp hydra_sim_NOBA.pin 
-./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp nu.pin
+#./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp nu.pin
+#./hydra_sim -ind hydra_sim_NOBA_5bin.dat -nohess -ainp nu.pin
+./hydra_sim -ind hydra_sim_NOBA_5bin.dat -nohess -ainp hydra_sim_NOBA_5bin.pin
 #-maxfn 30
 # -dd 1
