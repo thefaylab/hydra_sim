743fbedb122671c023352b8c1347a4b92e36cd7d
gavinfay <gfay42@gmail.com>
2022-09-19 11:07:03 -0400

    revises run script

--- run-hydra.sh
+++ run-hydra.sh
@@ -1,4 +1,4 @@
 #./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp hydra_sim_NOBA.pin 
-./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp nu.pin
+./hydra_sim -ind hydra_sim_NOBA.dat -nohess -ainp nu.pin -maxfn 1 -maxph 1
 #-maxfn 30
 # -dd 1
